30dacec3b08f41724cfa8d6fadc06977de7b8465
Hamza Khuswan <hamzakhuswan@outlook.com>
2020-11-01 12:34:30 +0100

    adding devtool, mode and stats

--- webpack.config.js
+++ webpack.config.js
@@ -6,98 +6,113 @@ const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-    module: {
-        rules: [
-            // JavaScript
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: [{
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ["@babel/preset-env"],
-                        plugins: [
-                            "@babel/plugin-proposal-class-properties",
-                            "@babel/plugin-transform-runtime"
-                        ]
-                    }
-                }],
-            },
-            // Handlebars
-            {
-                test: /\.hbs$/,
-                exclude: /node_modules/,
-                use: [
-                    {
-                        loader: "handlebars-loader",
+module.exports = env => {
+    let devtool, mode, stats;
+    if (env.production !== undefined && env.production === true) {
+        devtool = 'hidden-source-map';
+        mode = 'production';
+        stats = 'none';
+    } else {
+        devtool = 'eval';
+        mode = 'development';
+        stats = 'minimal';
+    }
+    return {
+        devtool,
+        mode,
+        stats,
+        module: {
+            rules: [
+                // JavaScript
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: [{
+                        loader: 'babel-loader',
                         options: {
-                            partialDirs: path.resolve(__dirname, "./src/components"),
-
+                            presets: ["@babel/preset-env"],
+                            plugins: [
+                                "@babel/plugin-proposal-class-properties",
+                                "@babel/plugin-transform-runtime"
+                            ]
                         }
-                    }
-                ],
-            },
-            // Scss
-            {
-                test: /\.s[ac]ss$/i,
-                exclude: /node_modules/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'sass-loader'
-                ],
-            },
-            // Files
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                exclude: /node_modules/,
-                use: [
-                    'file-loader',
-                ],
-                type: 'asset/resource'
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                exclude: /node_modules/,
-                use: [
-                    'file-loader',
-                ],
-                type: 'asset/inline'
-            },
-        ],
-    },
-    optimization: {
-        splitChunks: {
-            chunks: 'all',
+                    }],
+                },
+                // Handlebars
+                {
+                    test: /\.hbs$/,
+                    exclude: /node_modules/,
+                    use: [
+                        {
+                            loader: "handlebars-loader",
+                            options: {
+                                partialDirs: path.resolve(__dirname, "./src/components"),
+
+                            }
+                        }
+                    ],
+                },
+                // Scss
+                {
+                    test: /\.s[ac]ss$/i,
+                    exclude: /node_modules/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        'css-loader',
+                        'sass-loader'
+                    ],
+                },
+                // Files
+                {
+                    test: /\.(png|svg|jpg|gif)$/,
+                    exclude: /node_modules/,
+                    use: [
+                        'file-loader',
+                    ],
+                    type: 'asset/resource'
+                },
+                {
+                    test: /\.(woff|woff2|eot|ttf|otf)$/,
+                    exclude: /node_modules/,
+                    use: [
+                        'file-loader',
+                    ],
+                    type: 'asset/inline'
+                },
+            ],
         },
-    },
-    entry: [
-        path.resolve(__dirname, './src/index.js'),
-    ],
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: '[name].bundle.js',
-    },
-    plugins: [
-        new CopyPlugin({
-            patterns: [{ from: "public", to: "./" }]
-        }),
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, "./src/index.hbs"),
-            filename: 'index.html'
-        }),
-        new BrowserSyncPlugin({
-            host: "localhost",
-            port: 3000,
-            server: {
-                baseDir: [path.resolve(__dirname, './dist')]
+        optimization: {
+            splitChunks: {
+                chunks: 'all',
             },
-        }),
-        new FriendlyErrorsWebpackPlugin(),
-        new CleanWebpackPlugin({
-            cleanStaleWebpackAssets: false
-        }),
-        new MiniCssExtractPlugin()
-    ]
+        },
+        entry: [
+            path.resolve(__dirname, './src/index.js'),
+        ],
+        output: {
+            path: path.resolve(__dirname, './dist'),
+            filename: '[name].bundle.js',
+        },
+        plugins: [
+            new CopyPlugin({
+                patterns: [{ from: "public", to: "./" }]
+            }),
+            new HtmlWebpackPlugin({
+                template: path.resolve(__dirname, "./src/index.hbs"),
+                filename: 'index.html'
+            }),
+            new BrowserSyncPlugin({
+                host: "localhost",
+                port: 3000,
+                server: {
+                    baseDir: [path.resolve(__dirname, './dist')]
+                },
+            }),
+            new FriendlyErrorsWebpackPlugin(),
+            new CleanWebpackPlugin({
+                cleanStaleWebpackAssets: false
+            }),
+            new MiniCssExtractPlugin()
+        ]
+    }
 };
