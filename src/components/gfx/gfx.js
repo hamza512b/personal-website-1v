af934a68b9ba727116aec181257928bb91638519
Hamza Khuswan <hamzakhuswan@outlook.com>
2021-03-29 18:17:10 +0200

    remove cursor

--- src/components/gfx/gfx.js
+++ src/components/gfx/gfx.js
@@ -6,34 +6,28 @@ function randomInRange(a, b) {
     return Math.abs(Math.round(a + Math.random() * (b - a)));
 }
 
-function play_animation(i, delay) {
+
+let timeoutId;
+let i = 0;
+let delay = false;
+function play_animation(i) {
     if (i < length) {
         const element = elements[i];
-        const isCursor = element.dataset.fliker === "true";
-        setTimeout(() => {
+        timeoutId = setTimeout(() => {
+            element.classList.add("visable")
 
-            if (isCursor) {
-                element.classList.add("flikering")
-            } else {
-                element.classList.add("visable")
-            }
-
-            play_animation(i + 1, isCursor ? 4400 : randomInRange(300, 600))
-        }, delay);
-
+            play_animation(i + 1)
+        }, randomInRange(300, 1000));
     } else {
-        for (let j = 0; j < length; j++) {
-            const element = elements[j];
-            const isCursor = element.dataset.fliker === "true";
-            if (isCursor) {
-                element.classList.remove("flikering")
-            } else {
+        setTimeout(() => {
+            for (let j = 0; j < length; j++) {
+                const element = elements[j];
                 element.classList.remove("visable")
             }
-        }
-        
-        play_animation(0, 1000)
+
+            play_animation(0)
+        }, randomInRange(2000, 5000))
     }
 }
 
-play_animation(0, 1000);
+play_animation(0)
